2b8ff340d28e7267af89e211d08a1a76a39d1381
Kachiduck <kachiamaonwu@gmail.com>
2025-11-17 09:24:16 -0800

    FINISHED
    I COMPLETED IT. IT RUNS PERFECTLY

--- SentinelSum.java
+++ SentinelSum.java
@@ -9,14 +9,17 @@ public class SentinelSum
       
       System.out.print("Please enter a number to average in or -1 to stop: ");
       int number = scan.nextInt();
-      int sum = 0;      
+      int sum = 0;
+      sum = number + sum;      
       
       while (number != -1)
       {
          System.out.println("You entered " + number);
          System.out.println("Please enter another number or -1 to stop: ");
          number = scan.nextInt();
-         sum = number + sum;
+         if (number != -1)
+            sum = number + sum;
+                     
       }
       if (number == -1) {
          System.out.print("" + sum);
